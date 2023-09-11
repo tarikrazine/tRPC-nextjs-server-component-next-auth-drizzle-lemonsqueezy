679698713778c4cf673d12adc0d8cd44c46401f3
tarik razine <tarikrazine.com@gmail.com>
2023-09-11 21:16:14 +0100

    starting account settings

--- src/app/account/layout.tsx
+++ src/app/account/layout.tsx
@@ -1,32 +1,16 @@
 import Link from "next/link";
 
 import {
-    Card,
-    Grid,
     Title,
-    Text,
-    Tab,
-    TabList,
-    TabGroup,
-    TabPanel,
-    TabPanels,
   } from "@tremor/react";
 
 export default function AccountLayout(props: { children: React.ReactNode }) {
   return (
     <div className="py-8 px-4 max-w-screen-xl lg:px-6 flex flex-col justify-center items-center">
         <Title>Account Settings</Title>
-
-      <TabGroup className="mt-6">
-        <TabList>
-          <Link href="/account/profile"><Tab>Profile</Tab></Link>
-          <Link href="/account/password"><Tab>Password</Tab></Link>
-          <Link href="/account/billing"><Tab>Billing</Tab></Link>
-        </TabList>
-        <TabPanels>
-          {props.children}
-        </TabPanels>
-      </TabGroup>
+          <Link href="/account/profile">Profile</Link>
+          <Link href="/account/password">Password</Link>
+          <Link href="/account/billing">Billing</Link>
     </div>
   );
 }
--- src/app/account/profile/page.tsx
+++ src/app/account/profile/page.tsx
@@ -1,30 +1,9 @@
-import { Card, TabPanel, Grid, Text } from "@tremor/react"
+import { Text } from "@tremor/react"
 
 export default function ProfilePage() {
     return (
         <>
-          <TabPanel>
             <Text>Profile</Text>
-            <Grid numItemsMd={2} numItemsLg={3} className="gap-6 mt-6">
-              <Card>
-                {/* Placeholder to set height */}
-                <div className="h-28 bg-red-300" />
-              </Card>
-              <Card>
-                {/* Placeholder to set height */}
-                <div className="h-28 bg-red-300" />
-              </Card>
-              <Card>
-                {/* Placeholder to set height */}
-                <div className="h-28 bg-red-300" />
-              </Card>
-            </Grid>
-            <div className="mt-6">
-              <Card>
-                <div className="h-80 bg-red-300" />
-              </Card>
-            </div>
-          </TabPanel>
         </>
       );
 }
